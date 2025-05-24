f5f9ccdc6cf87a1fb69460d631ff7dd90fdbc3df
aktheraqila <aktheraqila@gmail.com>
2025-05-24 23:04:39 +0600

    Refactor layout to use ScrollView and add StartersActivity

--- The_Hungry_Developer_resturant_3/app/src/main/java/com/aas/the_hungry_developer_resturant_3/MainActivity.java
+++ The_Hungry_Developer_resturant_3/app/src/main/java/com/aas/the_hungry_developer_resturant_3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.aas.the_hungry_developer_resturant_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         startersCArd = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCArd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+
+                startActivity(startersActivityIntent);
+            }
+        });
 
     }
 }
---
+++ The_Hungry_Developer_resturant_3/app/src/main/java/com/aas/the_hungry_developer_resturant_3/StartersActivity.java
@@ -0,0 +1,24 @@
+package com.aas.the_hungry_developer_resturant_3;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StartersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_starters);
+
+        ListView starterList = findViewById(R.id.list_view_starters);
+
+
+    }
+}
