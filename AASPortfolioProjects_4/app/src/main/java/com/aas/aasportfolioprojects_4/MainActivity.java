5f8a3e6f55a67b1ef87d6cfd812952a5b6a588c3
aktheraqila <aktheraqila@gmail.com>
2025-05-30 03:40:05 +0600

    Add MainActivity, Project class, and layout files for project display

---
+++ AASPortfolioProjects_4/app/src/main/java/com/aas/aasportfolioprojects_4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.aas.aasportfolioprojects_4;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Project[] projects = {
+                new Project("Getting Started", "Our very first project, the default 'Hello world' project", R.drawable.getting_started),
+                new Project("AAS Quote", "Simple quote project, personal motivational quote for software developers", R.drawable.quote),
+                new Project("BMI Calculator", "Real life BMI calculator, calculates BMI for all ages and genders", R.drawable.calculator),
+                new Project("Simply Calculate", "Simple converter, converts inches to meters", R.drawable.tape),
+                new Project("The Hungry Developer", "A menu app for fictional restaurant, startes, main courses and desserts", R.drawable.hungry_developer)
+        };
+
+    }
+}
