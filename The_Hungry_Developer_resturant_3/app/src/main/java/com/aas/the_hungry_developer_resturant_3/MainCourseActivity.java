c606b8a930a6edf4b27caaa805b6f3aabf751ff7
aktheraqila <aktheraqila@gmail.com>
2025-05-29 23:11:16 +0600

    Add MainCourseActivity and layout for displaying main courses

---
+++ The_Hungry_Developer_resturant_3/app/src/main/java/com/aas/the_hungry_developer_resturant_3/MainCourseActivity.java
@@ -0,0 +1,15 @@
+package com.aas.the_hungry_developer_resturant_3;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_courses);
+    }
+}
