69a7eb668ac22448e82af31402bf4d7063154074
aktheraqila <aktheraqila@gmail.com>
2025-06-01 01:21:07 +0600

    Refactor ProjectsAdapter methods with comments for clarity

--- AASPortfolioProjects_4/app/src/main/java/com/aas/aasportfolioprojects_4/ProjectsAdapter.java
+++ AASPortfolioProjects_4/app/src/main/java/com/aas/aasportfolioprojects_4/ProjectsAdapter.java
@@ -9,18 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.ProjectViewHolder> {
 
 
+
     @NonNull
     @Override
+    //This is where we create the view holder
     public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
+    //This is where we bind the data to the view holder
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
 
     }
 
     @Override
+    //This is where we get the size of the list
     public int getItemCount() {
         return 0;
     }
