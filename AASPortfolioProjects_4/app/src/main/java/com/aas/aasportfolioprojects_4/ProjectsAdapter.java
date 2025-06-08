8d0f9f58311c052a5df496a7df127975f50305a7
aktheraqila <aktheraqila@gmail.com>
2025-06-08 23:48:51 +0600

    Enhance ProjectsAdapter to bind project data in ViewHolder

--- AASPortfolioProjects_4/app/src/main/java/com/aas/aasportfolioprojects_4/ProjectsAdapter.java
+++ AASPortfolioProjects_4/app/src/main/java/com/aas/aasportfolioprojects_4/ProjectsAdapter.java
@@ -3,6 +3,8 @@ package com.aas.aasportfolioprojects_4;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,14 +34,26 @@ public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.Projec
     @Override
 
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
-
+    holder.bind(projects[position]);
     }
 
 
     static class ProjectViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView appImage;
+        TextView appTitle;
+        TextView appDescription;
         public ProjectViewHolder(@NonNull View itemView) {
             super(itemView);
+            appImage = itemView.findViewById(R.id.image_view_project_icon);
+            appTitle = itemView.findViewById(R.id.text_view_project_title);
+            appDescription = itemView.findViewById(R.id.text_view_project_description);
+        }
+
+        public void bind(Project project){
+            appTitle.setText(project.name);
+            appDescription.setText(project.description);
+            appImage.setImageResource(project.image);
         }
     }
 
