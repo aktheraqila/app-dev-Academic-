e60d95d103eea8367d1e34699ea5c37609f2ad87
aktheraqila <aktheraqila@gmail.com>
2025-06-21 01:39:26 +0600

    Add activities for Places to Go and Things to Do with corresponding layouts

--- BucketList/app/src/main/java/com/aas/bucketlist/MainActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/MainActivity.java
@@ -1,8 +1,6 @@
 package com.aas.bucketlist;
 
-
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -11,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);    }
+        setContentView(R.layout.activity_main);
+    }
 }
