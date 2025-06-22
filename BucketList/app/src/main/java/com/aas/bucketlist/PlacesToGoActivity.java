28778a453105fb8c112f539e6517fff777bb49f9
aktheraqila <aktheraqila@gmail.com>
2025-06-22 21:35:14 +0600

    Remove EdgeToEdge.enable() from PlacesToGoActivity and ThingsToDoActivity

--- BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
@@ -13,7 +13,6 @@ public class PlacesToGoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_places_to_go);
 
     }
