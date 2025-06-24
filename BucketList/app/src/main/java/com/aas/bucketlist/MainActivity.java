1977e15fcef7eb08fc9b8a4529add1dffa098a8c
aktheraqila <aktheraqila@gmail.com>
2025-06-25 01:09:57 +0600

    Add click listeners for card views in MainActivity to navigate to PlacesToGo and ThingsToDo activities

--- BucketList/app/src/main/java/com/aas/bucketlist/MainActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/MainActivity.java
@@ -1,7 +1,11 @@
 package com.aas.bucketlist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
+        CardView thingsToDoCard = findViewById(R.id.card_view_things_to_do);
+        CardView placesToGo = findViewById(R.id.card_view_places_to_go);
+
+        thingsToDoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        placesToGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
