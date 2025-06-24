c02fc0dc63a2fd7a549933e0d6ff196890af7973
aktheraqila <aktheraqila@gmail.com>
2025-06-24 22:27:08 +0600

    Remove unused imports from PlacesToGoActivity

--- BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
@@ -2,11 +2,9 @@ package com.aas.bucketlist;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class PlacesToGoActivity extends AppCompatActivity {
 
