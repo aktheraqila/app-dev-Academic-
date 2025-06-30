145228ce5d6bd6379800b68fdeabe9c3e223e9ec
aktheraqila <aktheraqila@gmail.com>
2025-07-01 02:23:21 +0600

    Add BucketListEntry model and adapter for RecyclerView integration in activities

--- BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/PlacesToGoActivity.java
@@ -12,6 +12,18 @@ public class PlacesToGoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_to_go);
+        setupList();
 
+    }
+
+    private void setupList() {
+        BucketListEntry[] placesToGo = {
+                new BucketListEntry("Vietnam", "Con Dao Islands, Hanoi, Halong Bay, Hoi An, Lang Co", R.drawable.vietnam,4),
+                new BucketListEntry("Kerala", "Try varied tea flavours, stay in housevoat, the fabulous food!", R.drawable.kerala,3.5f),
+                new BucketListEntry("Japan", "Hot Springs, Shushi, Bamboo Forest, Bullet train through mountains", R.drawable.japan,4),
+                new BucketListEntry("Iceland", "Dyjandi Waterfall, nature reserve, maybe the Northern lights too!", R.drawable.iceland,4.7f),
+                new BucketListEntry("The amazzon, Brazil", "Try to survive being scared by all the creepy crawlies!", R.drawable.amazon,3.8f)
+
+        };
     }
 }
--- BucketList/app/src/main/java/com/aas/bucketlist/ThingsToDoActivity.java
+++ BucketList/app/src/main/java/com/aas/bucketlist/ThingsToDoActivity.java
@@ -11,6 +11,18 @@ public class ThingsToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_things_to_do);
+        setupList();
 
+    }
+
+    private void setupList() {
+        BucketListEntry[] thingstoDo = {
+                new BucketListEntry("Climb Mt Kilimanjaro", "Do it difficult way", R.drawable.kilimanjaro,4.5f),
+                new BucketListEntry("Experience the Northern Lights", "Somewhere in the arctic circle,probably Norway ", R.drawable.northern_lights,5),
+                new BucketListEntry("Road Trip Across USA", "Hire a car from West Cost, and travel to the east coast", R.drawable.road_trip,3),
+                new BucketListEntry("Scuba Dive", "In Koh Tao, thailand", R.drawable.scubadive,3.7f),
+                new BucketListEntry("Skydive", "Preferbly over somewhere with an amazing view", R.drawable.skydive,4.8f)
+
+        };
     }
 }
