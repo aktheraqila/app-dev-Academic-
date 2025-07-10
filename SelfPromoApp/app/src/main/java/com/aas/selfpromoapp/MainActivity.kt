c92a1ec4a2a00d7b5e951a01b04a07f8e890a9d3
aktheraqila <aktheraqila@gmail.com>
2025-07-11 02:56:52 +0600

    Rename preview button and implement click listener for preview functionality

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.aas.selfpromoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.aas.selfpromoapp.R.layout.activity_main
+import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +13,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+
+            onPReviewClicked()
+        }
+
+    }
+
+    private fun onPReviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name);
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number);
 
     }
 }
