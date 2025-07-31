8b646ef32e9c8ee6baebc944991113f0d28f9f3d
aktheraqila <aktheraqila@gmail.com>
2025-08-01 02:02:47 +0600

    Refactor onPreviewClicked to remove commented code and streamline data collection

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun onPreviewClicked() {
 
-        // Collect data from the input fields
             val contactName = binding.editTextContactName.text.toString()
             val contactNumber = binding.editTextContactNumber.text.toString()
             val displayName = binding.editTextMyDisplayName.text.toString()
@@ -30,7 +29,6 @@ class MainActivity : AppCompatActivity() {
             val immediateStart = binding.checkBoxImmediateStart.isChecked
             val startDate = binding.editTextStartDate.text.toString()
 
-            // Display the collected data in a Toast for testing purposes
             val testString = "Contact Name: $contactName, Contact Number: $contactNumber, My Display Name: $displayName, Include Junior: $includeJunior, Job Title: $jobTitle, Immediate Start: $immediateStart, Start Date: $startDate"
             Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
         }
