2b87a2dff3572afbc60d711baad731eab37c9114
aktheraqila <aktheraqila@gmail.com>
2025-08-24 00:21:31 +0600

    Refactor PreviewActivity to use ViewBinding and update layout initialization

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
@@ -2,13 +2,17 @@ package com.aas.selfpromoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aas.selfpromoapp.databinding.ActivityPreviewBinding
 
 
 class PreviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPreviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preview)
-        //text_view_message.text = "We are manually setting some text in our Textview just so we can see something!"
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textViewMessage.text = "We are manually setting some text in our Textview just so we can see something!"
 
  }
     }
