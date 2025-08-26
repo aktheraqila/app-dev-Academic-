dbd448f6a1216ec41b2fdf8b16fc029f4edf3b71
aktheraqila <aktheraqila@gmail.com>
2025-08-26 21:53:04 +0600

    Retrieve additional contact details from intent in PreviewActivity

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
@@ -15,5 +15,12 @@ class PreviewActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.textViewMessage.text = "We are manually setting some text in our Textview just so we can see something!"
 
+        val contactName: String? = intent.getStringExtra("Contact Name")
+        val contactNumber: String? = intent.getStringExtra("Contact Number")
+        val mydisplayName: String? = intent.getStringExtra("My Display")
+        val includeJunior: Boolean = intent.getBooleanExtra("Include Junior", false)
+        val jobTitle: String? = intent.getStringExtra("Job Title")
+        val immediateStart: Boolean = intent.getBooleanExtra("Immediate Start", false)
+        val startDate: String? = intent.getStringExtra("Start Date")
  }
     }
