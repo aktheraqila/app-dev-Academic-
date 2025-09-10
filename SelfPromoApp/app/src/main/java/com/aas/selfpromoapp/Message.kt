d1e64e8bc433f4913e0c6412d2d5a2a4ae9328d4
aktheraqila <aktheraqila@gmail.com>
2025-09-10 23:25:17 +0600

    Add comment to define properties in Message data class

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/Message.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/Message.kt
@@ -1,6 +1,7 @@
 package com.aas.selfpromoapp
 
 data class Message (
+    // Define the properties of the Message data class
     var contactName: String,
     var contactNumber: String ,
     var displayName: String ,
