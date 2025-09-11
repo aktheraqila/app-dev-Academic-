be4feeb1aeda2bb6c429e3cb00efc51591462ba5
aktheraqila <aktheraqila@gmail.com>
2025-09-11 23:27:11 +0600

    Removed data class Message in Message.kt

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/Message.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/Message.kt
@@ -1,12 +1,3 @@
 package com.aas.selfpromoapp
-
-data class Message (
-    // Define the properties of the Message data class
-    var contactName: String,
-    var contactNumber: String ,
-    var displayName: String ,
-    var includeJunior: Boolean ,
-    var jobTitle: String ,
-    var immediateStart: Boolean ,
-    var startDate: String
-    )
+class Message {
+}
