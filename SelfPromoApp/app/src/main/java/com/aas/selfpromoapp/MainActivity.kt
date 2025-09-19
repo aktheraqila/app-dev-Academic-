cd5394c5a3c7a66199a95e287b82875f11473188
aktheraqila <aktheraqila@gmail.com>
2025-09-19 23:56:16 +0600

    Pass Message object to PreviewActivity via intent extras

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             )
 
             val previewActivityIntent = Intent(this, PreviewActivity::class.java)
-
+            previewActivityIntent.putExtra("Message", message)
             startActivity(previewActivityIntent)
 
 
