f2eeb6a7c2ac9c50ffa5560b0f1c6d9e2a5fa8fc
aktheraqila <aktheraqila@gmail.com>
2025-10-04 23:27:56 +0600

    Refactor PreviewActivity to improve message preview formatting

--- SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
+++ SelfPromoApp/app/src/main/java/com/aas/selfpromoapp/PreviewActivity.kt
@@ -12,20 +12,24 @@ class PreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        displayFunction()
+    }
+
+    private fun displayFunction() {
         val message: Message = intent.getStringExtra("Message") as Message
         val messagePreviewText = """
-            Hi ${message.contactName},
-            
-            My name is ${message.displayName} and I am ${message.getFullJobDescription()}.
-            
-            I have a portfolio of apps to demonstrate my technical skills that I can show on request.
-            
-            I am able to start a new position ${message.getAvailabitity()}.
-            
-            Thanks and best regards.
-        """.trimIndent()
+                Hi ${message.contactName},
+                
+                My name is ${message.displayName} and I am ${message.getFullJobDescription()}.
+                
+                I have a portfolio of apps to demonstrate my technical skills that I can show on request.
+                
+                I am able to start a new position ${message.getAvailabitity()}.
+                
+                Thanks and best regards.
+            """.trimIndent()
 
 
-      binding.textViewMessage.text = messagePreviewText
+        binding.textViewMessage.text = messagePreviewText
     }
-    }
+}
