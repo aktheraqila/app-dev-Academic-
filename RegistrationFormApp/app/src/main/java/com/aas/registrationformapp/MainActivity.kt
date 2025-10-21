2a6812e4042379610672318220d33ff3ae7cb434
aktheraqila <aktheraqila@gmail.com>
2025-10-21 23:22:47 +0600

    Add spinner for title selection and bind UI elements in MainActivity

--- RegistrationFormApp/app/src/main/java/com/aas/registrationformapp/MainActivity.kt
+++ RegistrationFormApp/app/src/main/java/com/aas/registrationformapp/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.aas.registrationformapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aas.registrationformapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupSpinner()
+        setupButton()
+
+    }
+
+
+    private fun setupSpinner() {
+
+        val titles = arrayOf("Miss", "Mrs", "Ms", "Mr", "Dr", "Prof")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, titles)
+        binding.spinnerTitle.adapter = titleAdapter
+    }
+
+    private fun setupButton() {
+        binding.buttonCreateAccount.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount(){
 
     }
 }
