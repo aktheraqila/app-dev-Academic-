6d1256cadf741b230378cf1526d2d9e0116ab371
aktheraqila <aktheraqila@gmail.com>
2025-10-26 22:22:18 +0600

    Implement SummaryActivity with user data retrieval and display functionality

--- RegistrationFormApp/app/src/main/java/com/aas/registrationformapp/SummaryActivity.kt
+++ RegistrationFormApp/app/src/main/java/com/aas/registrationformapp/SummaryActivity.kt
@@ -1,6 +1,8 @@
 package com.aas.registrationformapp
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
@@ -32,9 +34,23 @@ class SummaryActivity : AppCompatActivity() {
 
     private fun displayUser() {
         binding.textViewUserFullName.text = user.getFullName()
+        binding.textViewEmailAddress.text = user.email
+        binding.textViewPhhone.text = user.phone
     }
 
     private fun setupClickListener() {
+        binding.textViewEmailAddress.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto${user.email}")
+            startActivity(intent)
+        }
+
+        binding.textViewPhhone.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${user.phone}")
+            startActivity(intent)
+
+        }
 
     }
 }
