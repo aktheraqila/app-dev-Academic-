736d1f1da170003607c0cf0623138b2de8ca1c22
aktheraqila <aktheraqila@gmail.com>
2025-12-01 22:38:40 +0600

    Initialize bottom navigation and frame content variables in MainActivity

--- RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.aas.recordkeeper
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.aas.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var frameContent: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.bottomNav = findViewById(R.id.bottom_nav)
-        binding.frameContent
+        binding.frameContent  = findViewById(R.id.frame_content)
     }
 }
