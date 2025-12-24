af494b24ff5865db6e05e46bc3a46825045ecb83
aktheraqila <aktheraqila@gmail.com>
2025-12-24 21:41:13 +0600

    Implement View.OnClickListener in MainActivity and update buttonCycling click handling

--- RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
@@ -2,13 +2,14 @@ package com.aas.recordkeeper
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.aas.recordkeeper.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
     private lateinit var binding: ActivityMainBinding
 
 
@@ -17,9 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val theClickListener: View.OnClickListener= MyClickListener()
-        binding.buttonCycling.setOnClickListener(theClickListener)
+        binding.buttonCycling.setOnClickListener()
 
+    }
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(this, "I have made the Activity a click listener!", Toast.LENGTH_SHORT)
     }
 
     private fun onlyCyclingClicked() {
