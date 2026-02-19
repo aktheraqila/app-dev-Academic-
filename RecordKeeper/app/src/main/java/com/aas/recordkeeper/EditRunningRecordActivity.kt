8c287b70a655d3358972fa0c74e34b84bfd17f3c
aktheraqila <aktheraqila@gmail.com>
2026-02-19 23:36:57 +0600

    Refactor EditRunningRecordActivity to use ViewBinding and simplify window insets handling

--- RecordKeeper/app/src/main/java/com/aas/recordkeeper/EditRunningRecordActivity.kt
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/EditRunningRecordActivity.kt
@@ -2,19 +2,19 @@ package com.aas.recordkeeper
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+import com.aas.recordkeeper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditRunningRecordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_edit_running_record)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
