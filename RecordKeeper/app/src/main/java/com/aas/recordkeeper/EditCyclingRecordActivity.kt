cec707302148835dd4c07eb4acb1fa6f70bf830c
aktheraqila <aktheraqila@gmail.com>
2026-02-21 03:31:28 +0600

    Add EditCyclingRecordActivity and layout for editing cycling records

---
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/EditCyclingRecordActivity.kt
@@ -0,0 +1,22 @@
+package com.aas.recordkeeper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+import com.aas.recordkeeper.databinding.ActivityEditCyclingRecordBinding
+
+class EditCyclingRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditCyclingRecordBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditCyclingRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val distance = intent.getStringExtra("Distance")
+        title = "$distance Record"
+
+    }
+}
