6c60be354fa73fb8b67ac7a13c4fe3cddc3aeb33
aktheraqila <aktheraqila@gmail.com>
2026-02-22 06:12:51 +0600

    Remove unused imports in EditCyclingRecordActivity

--- RecordKeeper/app/src/main/java/com/aas/recordkeeper/EditCyclingRecordActivity.kt
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/EditCyclingRecordActivity.kt
@@ -1,11 +1,7 @@
 package com.aas.recordkeeper
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.viewbinding.ViewBinding
 import com.aas.recordkeeper.databinding.ActivityEditCyclingRecordBinding
 
 class EditCyclingRecordActivity : AppCompatActivity() {
