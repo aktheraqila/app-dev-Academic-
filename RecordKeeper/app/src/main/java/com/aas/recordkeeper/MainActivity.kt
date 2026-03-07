c1fc1161261c6a383746ddba61673e0ee79f2f12
aktheraqila <aktheraqila@gmail.com>
2026-03-07 23:46:31 +0600

    Remove unused import in MainActivity.kt for cleaner code

--- RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
+++ RecordKeeper/app/src/main/java/com/aas/recordkeeper/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.aas.recordkeeper
 
 import android.os.Bundle
-import android.text.TextUtils.replace
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
